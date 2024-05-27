516cfdab964b13c5bb2b09b25740e007d35b9ecb
CromyNews <esteemzileas@naver.com>
2024-05-27 14:58:30 +0900

    More Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Put,
+    Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,25 +17,31 @@ export class MoviesController {
         return 'This will return all movies';
     }
 
+    @Get('search')
+    search(@Query('year') searchingYear: string) {
+        return `We are searching for a movie with a title ${searchingYear}`;
+    }
+
     @Get('/:id')
     getOne(@Param('id') moviId: string) {
         return `This will return one movie ${moviId}`;
     }
 
     @Post()
-    create() {
-        return 'This will create to movie';
+    create(@Body() movieData) {
+        return movieData;
     }
 
     @Delete('/:id')
     remove(@Param('id') moviedId: string) {
         return `This will delete a movid => ${moviedId}`;
     }
-    
+
     @Patch('/:id')
-    path(@Param('id') moviId: string) {
-        return `This winll patch a movie with the id: ${moviId}`
+    path(@Param('id') moviId: string, @Body() updateData) {
+        return {
+            updataMovie: moviId,
+            ...updateData,
+        };
     }
-    
-    
 }
